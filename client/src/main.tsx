078655c62508bf753b6e0bc02f89eb0ac2aa5ed5
Jessica Tell <hello.jessicatell@gmail.com>
2024-04-22 13:13:58 +0200

    front end done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "../Router.tsx";
+import CartProvider from "./context/CartContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <CartProvider>
+    {/* <OrderProvider> */}
+    <RouterProvider router={router} />
+    {/* </OrderProvider> */}
+  </CartProvider>
+);
