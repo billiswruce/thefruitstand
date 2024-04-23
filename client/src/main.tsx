7cf501b8f2dd1f9a69cf7ce05f696df791ca395e
Jessica Tell <hello.jessicatell@gmail.com>
2024-04-23 12:12:20 +0200

    create new client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,10 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "../Router.tsx";
-import CartProvider from "./context/CartContext.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <CartProvider>
-    {/* <OrderProvider> */}
-    <RouterProvider router={router} />
-    {/* </OrderProvider> */}
-  </CartProvider>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
