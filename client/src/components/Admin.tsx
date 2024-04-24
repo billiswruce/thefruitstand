7a477f509772c5c6aa3e1a8425646c811c26b5f1
Jessica Tell <hello.jessicatell@gmail.com>
2024-04-24 14:40:20 +0200

    add product works to DB from client works

--- client/src/components/Admin.tsx
+++ client/src/components/Admin.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AddProduct } from "./AddProduct";
-import { IProduct } from "../models/IProduct";
+import { ICreateProduct } from "../models/IProduct";
 
 export const Admin = () => {
   const [showAddModal, setShowAddModal] = useState(false);
@@ -9,14 +9,30 @@ export const Admin = () => {
     setShowAddModal(!showAddModal);
   };
 
-  const handleAddProduct = async (product: IProduct) => {
-    //Lägg till logiken för att lägga till produkter här.
-    console.log(product);
+  const handleAddProduct = async (product: ICreateProduct) => {
+    try {
+      const response = await fetch("/api/create-product", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Product added:", data);
+      } else {
+        console.error("Failed to add product:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error adding product:", error);
+    }
   };
 
   return (
     <>
-      <h3>Admin</h3>
+      <h1>Admin</h1>
       <button onClick={handleToggleAddModal}>Add new Product</button>
       <AddProduct
         open={showAddModal}
